19c6015922a9184e3bb506f2d13723f4f151d44e
Raheem Carridice <carridiceraheem871@gmail.com>
2023-04-11 04:08:26 -0500

    have api send html file and linked clientside js file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', function(req, res){
-    res.send('hello world');
+    res.sendFile('home.html');
 });
 
 app.listen(port, function(){
