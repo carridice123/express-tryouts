3899cfa2b3d7030a2e95e70f47ec305b464e4f25
Raheem Carridice <carridiceraheem871@gmail.com>
2023-04-11 16:44:17 -0500

    remove cors

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 
-app.use(cors)
 const port = process.env.PORT || 3000
  app.use(cors)
 app.get('/', function(req, res){
