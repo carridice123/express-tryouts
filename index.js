97f6a9b32c0e9b11a74b1a05cee9be2592a1196b
Raheem Carridice <carridiceraheem871@gmail.com>
2023-04-11 04:15:17 -0500

    add correct path to the  api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', function(req, res){
-    res.sendFile('home.html');
+    res.sendFile(__dirname + '/home.html');
 });
 
 app.listen(port, function(){
