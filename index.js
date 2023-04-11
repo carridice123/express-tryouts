39a4e6fd246fa46d442d4f485438fbf4b39de627
Raheem Carridice <carridiceraheem871@gmail.com>
2023-04-11 16:47:07 -0500

    fixed typo in index js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 const port = process.env.PORT || 3000
- app.use(cors)
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/home.html');
 });
