8b82275ad9b5bd8aff7100cdbdebcefa5a6e2767
Raheem Carridice <carridiceraheem871@gmail.com>
2023-04-13 03:58:41 -0500

    add user input client side js

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -1,1 +1,18 @@
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const rapperName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://rich-ruby-llama-gown.cyclic.app/api/${rapperName}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('h2').innerText = data.birthName
+    }catch(error){
+        console.log(error)
+    }
+}
+
+
+
 console.log('its working')
