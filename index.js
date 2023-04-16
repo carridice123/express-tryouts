fca5f4c7ff45cd8a6bf7655fb5b60f1aee3b400f
Raheem Carridice <carridiceraheem871@gmail.com>
2023-04-16 07:50:10 -0500

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 3000
 const homeRoutes = require('./routes/main')
 const apiRoutes = require('./routes/rapperApi')
+app.use(cors)
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
